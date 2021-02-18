9a696aa99c5dd3146c1256cb2d65e11bc84603ff
hirorocky <hirorocky@gmail.com>
2021-02-18 20:49:25 +0900

    2-2-3 Commentのコントローラー・ビュー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'communities#index'
-  resources :communities, only: %i[index new create show]
+  resources :communities, only: %i[index new create show] do
+    resources :comments, only: %i[new create]
+  end
 end
