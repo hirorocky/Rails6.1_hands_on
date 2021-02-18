da19def1689a8d998b8cb0a85bcc54d157744bc6
hirorocky <hirorocky@gmail.com>
2021-02-18 20:16:49 +0900

    2-2-2 Communityのコントローラー・ビュー

---
+++ app/controllers/communities_controller.rb
@@ -0,0 +1,29 @@
+class CommunitiesController < ApplicationController
+  def index
+    @communities = Community.all
+  end
+
+  def new
+    @community = Community.new
+  end
+
+  def create
+    @community = Community.new(community_params)
+    if @community.save
+      redirect_to communities_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @community = Community.find(params[:id])
+    @comments = @community.comments
+  end
+
+  private
+
+  def community_params
+    params.require(:community).permit(:title, :owner_name)
+  end
+end
