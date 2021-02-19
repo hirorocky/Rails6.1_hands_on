e1ece759f93ec64b3b356e7505a42698d6b0d9bf
hirorocky <hirorocky@gmail.com>
2021-02-19 22:23:56 +0900

    3-2, 3-3 Bootstrap, FontAwesomeを入れる

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,9 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import "channels"
+import '@fortawesome/fontawesome-free/js/all';
+import "bootstrap"
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
