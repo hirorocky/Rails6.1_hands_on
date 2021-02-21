18bcd0512815e941d1cc7fd536c93ab4c993b478
hirorocky <hirorocky@gmail.com>
2021-02-21 11:01:46 +0900

    3-4 日本語化

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,9 @@ module Rails61HandsOn
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
