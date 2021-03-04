4ba9b0716066b06d074d1755330887b28d69fc65
hirorocky <hirorocky@gmail.com>
2021-03-04 16:22:56 +0900

    4-3 セッション機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: 'communities#index'
+
+  get 'login' => 'user_sessions#new', as: :login
+  post 'login' => 'user_sessions#create'
+  post 'logout' => 'user_sessions#destroy', as: :logout
   resources :users
+
   resources :communities, only: %i[index new create show] do
     resources :comments, only: %i[new create]
   end
